9a736b1d8f43859bb448bf7da228d48ea957c367
Fluently <f_liuchang@163.com>
2017-09-16 14:04:16 +0800

    add log

--- 02.springBoot/workspace/myzone/src/main/java/fluently/myzone/controller/UserController.java
+++ 02.springBoot/workspace/myzone/src/main/java/fluently/myzone/controller/UserController.java
@@ -2,6 +2,8 @@ package fluently.myzone.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +16,8 @@ import fluently.myzone.service.IOperationUserService;
 @RequestMapping("/user")
 public class UserController {
 
+	protected static Logger logger = LoggerFactory.getLogger(UserController.class);
+	
 	@Autowired
 	private IOperationUserService iOperationUserService;
 
@@ -24,6 +28,9 @@ public class UserController {
 	
 	@RequestMapping(value = "/queryAllUser", method = RequestMethod.GET)
 	public List<UserVO> queryAllUser() {
-		return iOperationUserService.queryAllUser();
+		logger.info("queryAllUser");
+		List<UserVO> listUser = iOperationUserService.queryAllUser();
+		logger.info(listUser.toString());
+		return listUser;
 	}
 }
